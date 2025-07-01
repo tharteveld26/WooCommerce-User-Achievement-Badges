3ba8dfb043211c48d7fe8121012921df4fb6d488
tharteveld26 <128097236+tharteveld26@users.noreply.github.com>
2025-07-01 08:10:21 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
+set -e
+
+# Update packages
 apt-get update
-apt-get install -y php-cli
+
+# Install PHP CLI (Codex seems to already have it, but doesn't hurt to ensure)
+apt-get install -y php-cli php-mbstring unzip curl git
+
+# Optionally install WP CLI or Composer
+# curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+# chmod +x wp-cli.phar
+# mv wp-cli.phar /usr/local/bin/wp
+
+# Confirm installed PHP version
+php -v
